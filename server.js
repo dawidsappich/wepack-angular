23bc98c1173d6db2a8220de4b9020b2061b2297d
dawidsappich <dawidsappich@gmail.com>
2017-11-05 08:46:01 +0100

    add bodyparser

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+// body-parser for imcoming requests
+const bodyParser = require('body-parser');
 // logging framework
 const morgan = require('morgan');
-const PORT = 5489;
+const PORT = 3000;
 
 // use morgan for logging in development modus
 app.use(morgan('dev'));
 // serve static files from public folder
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // listen port to incoming requests
 app.listen(PORT, () => {
